e0fdeae5537e0db9e6ead71f040e39f10e91bf82
AlekseyDream <u550@list.ru>
2023-07-08 21:11:06 +0300

    14pr

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 const app = express();
 app.use(express.json());
-app.post('/signin', signinValidate, login);
 app.post('/signup', signupValidate, createUser);
-app.use(router);
+app.post('/signin', signinValidate, login);
 app.use(auth);
+app.use(router);
 app.use(helmet());
 app.use(errors());
 app.use((err, req, res, next) => {
