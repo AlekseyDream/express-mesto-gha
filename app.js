c638436bc245da75cba5381a1ddbe65a4ca263fd
AlekseyDream <u550@list.ru>
2023-07-08 12:12:29 +0300

    14pr5

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { createUser, login } = require('./controllers/users');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
+const { createUser, login } = require('./controllers/users');
+const { errorHandler } = require('./middlewares/errorHandler');
+const { signinValidate, signupValidate } = require('./middlewares/validation');
 
 const { PORT = 3000 } = process.env;
 
@@ -10,10 +14,13 @@ app.use(express.json());
 
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', signinValidate, login);
+app.post('/signup', signupValidate, createUser);
 app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use(helmet());
+app.use(errors());
+app.use(errorHandler);
 app.listen(PORT);
