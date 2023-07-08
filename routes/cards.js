bc5581d5effc83492468e011db79c34a1bbde2e1
AlekseyDream <u550@list.ru>
2023-07-08 17:00:59 +0300

    14pr

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {
-  getCards, createCard, deleteCard, likeCard, dislikeCard,
+  getCards, createCard, deleteCard, likeCard,
 } = require('../controllers/cards');
 const { cardIdValidate, cardValidate } = require('../middlewares/validation');
 
@@ -8,6 +8,6 @@ router.get('/', getCards);
 router.post('/', cardValidate, createCard);
 router.delete('/:cardId', cardIdValidate, deleteCard);
 router.put('/:cardId/likes', cardIdValidate, likeCard);
-router.delete('/:cardId/likes', cardIdValidate, dislikeCard);
+router.delete('/:cardId/likes', cardIdValidate, likeCard);
 
 module.exports = router;
